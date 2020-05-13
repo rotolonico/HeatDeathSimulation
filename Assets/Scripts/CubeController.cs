8e1cfe9d47d241879c1314cf8aa44f475115cbbc
rotolonico <rotolonico@gmail.com>
2020-05-13 15:46:50 +0200

    Initial Commit

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class CubeController : MonoBehaviour
+{
+    public float exchangeSpeed;
+
+    private Light2D light2D;
+    private Transform thisTransform;
+    private Transform textTransform;
+    private TextMeshPro text;
+    private Rigidbody2D rigidBody;
+
+    private float temperature;
+    private bool moving;
+
+    private void Start()
+    {
+        light2D = GetComponent<Light2D>();
+        thisTransform = transform;
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        textTransform = thisTransform.GetChild(0);
+        text = textTransform.GetComponent<TextMeshPro>();
+    }
+
+    private void Update()
+    {
+        textTransform.localEulerAngles = new Vector3(0, 0, 360 - thisTransform.eulerAngles.z);
+        textTransform.position = thisTransform.position + new Vector3(0, 1, 0);
+        if (!moving) return;
+        var thisTransformPosition = CubesController.MainCamera.ScreenToWorldPoint(Input.mousePosition);
+        thisTransformPosition.x = Mathf.Clamp(thisTransformPosition.x, -8, 8);
+        thisTransformPosition.y = Mathf.Clamp(thisTransformPosition.y, -2.5f, 5);
+        thisTransformPosition.z = 0;
+        thisTransform.position = thisTransformPosition;
+        var thisTransformRotation = thisTransform.eulerAngles;
+        thisTransformRotation += new Vector3(0, 0, Time.deltaTime * 100);
+        thisTransform.eulerAngles = thisTransformRotation;
+        rigidBody.velocity = Vector2.zero;
+    }
+
+    public void StartMoving() => moving = true;
+
+    public void StopMoving() => moving = false;
+
+    public void Destroy() => Destroy(gameObject);
+
+    public void SmudgeTemperature(float smudge)
+    {
+        temperature += smudge;
+        temperature = Mathf.Clamp(temperature, 0, 500);
+        UpdateTemperature();
+    }
+
+    public void ChangeTemperature(float value)
+    {
+        temperature = Mathf.Clamp(value, 0, 500);
+        UpdateTemperature();
+    }
+
+    private void UpdateTemperature()
+    {
+        light2D.color = Color.Lerp(Color.white, Color.red, temperature / 500);
+        text.text = $"T = {Math.Round(temperature)}K";
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (!other.collider.CompareTag("Cube")) return;
+        var otherController = other.collider.GetComponent<CubeController>();
+        if (Math.Abs(otherController.temperature - temperature) < 0.5f) return;
+        if (otherController.temperature > temperature)
+        {
+            otherController.SmudgeTemperature(-exchangeSpeed * Time.deltaTime);
+            SmudgeTemperature(exchangeSpeed * Time.deltaTime * (1 - CubesController.ExchangeDecay));
+        }
+        else
+        {
+            otherController.SmudgeTemperature(exchangeSpeed * Time.deltaTime * (1 - CubesController.ExchangeDecay));
+            SmudgeTemperature(-exchangeSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/CubesController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubesController : MonoBehaviour
+{
+    public static Camera MainCamera;
+    
+    public static float ExchangeDecay;
+
+    public GameObject cubePrefab;
+    
+    public Slider exchangeDecaySlider;
+    public TextMeshProUGUI exchangeDecaySliderText;
+
+    private void Awake() => MainCamera = Camera.main;
+
+    private void Start() => exchangeDecaySlider.onValueChanged.AddListener(ChangeExchangeDecay);
+
+    private void ChangeExchangeDecay(float value)
+    {
+        ExchangeDecay = value;
+        exchangeDecaySliderText.text = $"%D = {Math.Round(value, 2)}";
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(2)) SpawnCube();
+        else if (Input.GetMouseButtonDown(0)) StartMovingCube();
+        else if (Input.GetMouseButtonUp(0)) StopMovingCube();
+        else if (Input.GetMouseButtonDown(1)) DestroyCube();
+        else if (Math.Abs(Input.GetAxisRaw("Mouse ScrollWheel")) > 0.01f)
+            SmudgeCubeTemperature(Input.GetAxisRaw("Mouse ScrollWheel") * 50);
+    }
+
+    private void SpawnCube()
+    {
+        var position = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+        position.z = 0;
+        Instantiate(cubePrefab, position, Quaternion.identity);
+    }
+
+    private void StartMovingCube()
+    {
+        var cube = GetCube();
+        if (cube == null) return;
+        cube.StartMoving();
+    }
+
+    private void StopMovingCube()
+    {
+        var cube = GetCube();
+        if (cube == null) return;
+        cube.StopMoving();
+    }
+
+    private void SmudgeCubeTemperature(float smudge)
+    {
+        var cube = GetCube();
+        if (cube == null) return;
+        cube.SmudgeTemperature(smudge);
+    }
+
+    private void DestroyCube()
+    {
+        var cube = GetCube();
+        if (cube == null) return;
+        cube.Destroy();
+    }
+
+    private CubeController GetCube()
+    {
+        var hitObj = Physics2D.OverlapPoint(MainCamera.ScreenToWorldPoint(Input.mousePosition));
+        if (hitObj == null || !hitObj.CompareTag("Cube")) return null;
+        return hitObj.GetComponent<CubeController>();
+    }
+}
